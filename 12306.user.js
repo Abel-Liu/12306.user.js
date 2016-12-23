4e7696077d4b5fab9bc63fb0ad33341ad14e84fd
Abel Liu <abel.liu.china@outlook.com>
2016-12-23 18:31:01 +0800

    init

---
+++ 12306.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         12306 order helper
+// @namespace    http://abelliu.com
+// @version      0.1
+// @description  change order style and help query quickly
+// @author       abel
+// @require      https://code.jquery.com/jquery-2.2.2.min.js
+// @include      https://kyfw.12306.cn/*
+// @grant        none
+// ==/UserScript==
+
+function defocusRefunded() {
+    var orderDiv = $(".order-item");
+    for(var i=0; i < orderDiv.length; i++) {
+        var idx = orderDiv[i].innerHTML.indexOf("已退票");
+        if(idx > 0) {
+            $(orderDiv[i]).css("opacity","0.3");
+        }
+    }
+    
+    setTimeout(defocusRefunded,1000);
+}
+
+$(document).ready(function(){
+    defocusRefunded();
+});
